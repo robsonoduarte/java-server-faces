4ce644895890716612b19ea44cceaeed2a647a9e
robsonoduarte <robson.o.d@gmail.com>
2015-04-09 23:56:25 -0300

    add PersonMB and Person and integrate with xtml.

---
+++ jsf-bean-validation/src/main/java/br/com/mystudies/jsf/Person.java
@@ -0,0 +1,27 @@
+package br.com.mystudies.jsf;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+public class Person {
+
+	@NotNull
+	private String name;
+	@Min(value=1)
+	private Integer age;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+}
---
+++ jsf-bean-validation/src/main/java/br/com/mystudies/jsf/PersonMB.java
@@ -0,0 +1,28 @@
+package br.com.mystudies.jsf;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class PersonMB {
+
+	public Person person = new Person();
+
+	
+	public void save(){
+		
+	}
+	
+	
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+	
+	
+	
+}
