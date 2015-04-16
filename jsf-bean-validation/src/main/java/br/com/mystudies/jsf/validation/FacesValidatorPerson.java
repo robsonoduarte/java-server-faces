3e01b2959ec117ef3cfbfad667a6597866bbb415
Robson Duarte <robson.o.d@gmail.com>
2015-04-15 23:59:44 -0300

    add roles to add class error

--- jsf-bean-validation/src/main/java/br/com/mystudies/jsf/validation/FacesValidatorPerson.java
+++ jsf-bean-validation/src/main/java/br/com/mystudies/jsf/validation/FacesValidatorPerson.java
@@ -1,7 +1,5 @@
 package br.com.mystudies.jsf.validation;
 
-import java.util.Set;
-
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.component.html.HtmlInputText;
@@ -9,8 +7,6 @@ import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
 
 @FacesValidator(value="validatorPerson")
 public class FacesValidatorPerson implements Validator {
@@ -18,21 +14,14 @@ public class FacesValidatorPerson implements Validator {
 	@Override
 	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
 		
-		
 		HtmlInputText htmlInputText = (HtmlInputText) component;
 		
-		
-		javax.validation.Validator validator = 
-				Validation.buildDefaultValidatorFactory().getValidator();
-		
-		Set<ConstraintViolation<Object>> constraints = validator.validateProperty(value, component.getId());
-		
-		if(constraints.size() > 0){
-			htmlInputText.setStyleClass("error");			
+		if(value == null){
+			htmlInputText.setStyleClass(htmlInputText.getStyleClass().concat(" error"));
 			throw new ValidatorException(new FacesMessage());
+		}else{
+			htmlInputText.setStyleClass("form-control");
 		}
-		
-		
 	}
 
 }
